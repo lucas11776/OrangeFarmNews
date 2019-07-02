05248492f92ace553c2407f2a3ab1d33cd5886a5
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-07-02 04:04:04 -0700

    Added Account Model

--- application/models/Account_model.php
+++ application/models/Account_model.php
@@ -3,5 +3,104 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Account_model extends CI_Model
 {
+  /**
+   * Picture Director Reference
+   * 
+   * @var string
+   */
+  public const PICTURE_PATH = '/upload/news/picture/';
+
+  /**
+   * Defualt Picture
+   * 
+   * @var string
+   */
+  public const PICTURE = 'user.png';
+
+  /**
+   * Account Roles
+   * 
+   * @var array
+   */
+  public const ROLE = array(0 => 'guest', 1 => 'user', 2 => 'editor', 3 => 'administrator');
+
+  /**
+   * Get Account From Databae
+   * 
+   * @param   array
+   * @return  array
+   */
+  public function get(array $where)
+  {
+    return $this->db->get('accounts', $where)->result_array();
+  }
   
+  /**
+   * Add Account To Database
+   * 
+   * @param   array
+   * @return  boolean
+   */
+  public function create(array $account)
+  {
+    $data = array(
+      'username' => $account['username'],
+      'email'    => $account['email'],
+      'picture'  => $account['picture'] ?? $this::PICTURE_PATH.$this::PICTURE,
+      'role'     => 1, # set user role to user on account create
+      'name'     => $account['name']    ?? '',
+      'surname'  => $account['surname'] ?? '',
+      'password' => $account['password']
+    );
+    return $this->db->insert('accounts', $data);
+  }
+
+  /**
+   * Change/Update Account
+   * 
+   * @param   array
+   * @param   array
+   * @return  boolean
+   */
+  public function update(array $where,array $account)
+  {
+    $data = array(
+      'name'     => $account['name']    ?? '',
+      'surname'  => $account['surname'] ?? ''
+    );
+
+    // check if picture is upload
+    if(isset($account['picture']))
+    {
+      $data['picture'] = $account['picture'];
+    }
+
+    return $this->db->where($where)
+                    ->update('accounts', $account);
+  }
+
+  /**
+   * Change/Update Account Password
+   * 
+   * @param   array
+   * @param   array
+   * @return  boolean
+   */
+  public function change_password(array $where, string $password)
+  {
+    return $this->db->where($where)
+                    ->update('accounts', array('password' => $password));
+  }
+
+  /**
+   * Delete Account
+   * 
+   * @param   array
+   * @return  boolean
+   */
+  public function delete(array $where)
+  {
+    return $this->db->where($where)->delete('accounts');
+  }
+
 }
