a36d8acc5a4a9a90fc6d6a205c4914edf073ad6d
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-07-15 07:58:52 +1300

    Updated Account Controller

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -33,10 +33,30 @@ class Account extends CI_Controller
       'unread_messages' => $this->contect->get(array('seen' => 0), 5)
     );
 
+    # check update type
+    if(strtolower($this->input->post('type') ?? '') == 'password')
+    {
+      $this->change_password($page_details);
+    }
+    else
+    {
+      $this->update_details($page_details);
+    }
+
+  }
+
+  /**
+   * Update Account Details
+   *
+   * @param   array
+   * @return  void
+   */
+  private function update_details($page_details)
+  {
     # validate required data
     $this->form_validation->set_rules('name', 'name', 'min_length[3]|max_length[30]');
     $this->form_validation->set_rules('surname', 'surname', 'min_length[3]|max_length[30]');
-    $this->form_validation->set_rules('picture', 'profile picture', 'callback_upload_picture');
+    $this->form_validation->set_rules('picture', 'profile picture', 'callback_upload_profile_picture');
 
     if($this->form_validation->run() === false)
     {
@@ -44,5 +64,88 @@ class Account extends CI_Controller
 
       return;
     }
+
+    # updated data
+    $update = array(
+      'name'    => $this->input->post('name'),
+      'surname' => $this->input->post('surname')
+    );
+
+    # user ID
+    $where = array('id' => $this->auth->account('id'));
+
+    # update user account
+    if($this->account->update($where, $update) === false)
+    {
+      $this->session->set_flashdata('update_account_error', 'Something went wrong when tring to connect to database.');
+    }
+    else
+    {
+      $this->session->set_flashdata('account_updated', 'Account details updated.');
+    }
+
+    $this->view('update', $page_details);
   }
+
+  /**
+   * Change Account Password
+   *
+   * @param   array
+   * @return  void
+   */
+  private function change_password(array $page_details)
+  {
+    # check required data
+    $this->form_validation->set_rules('old_password', 'old password', 'required|callback_password_matches');
+    $this->form_validation->set_rules('new_password', 'new password', 'required|min_length[6]|max_length[20]');
+    $this->form_validation->set_rules('confirm_password', 'confirm password', 'required|matches[new_password]');
+
+    if($this->form_validation->run() === false)
+    {
+      $this->view('update', $page_details);
+
+      return;
+    }
+
+    $this->view('update', $page_details);
+  }
+
+  /**
+   * Update Profile Picture
+   *
+   * @return boolean
+   */
+  public function upload_profile_picture()
+  {
+    # check if picture isset
+    if(isset($_FILES['picture']))
+    {
+      return true;
+    }
+
+    return true;
+  }
+
+  /**
+   * Check If Password Match Account Password
+   *
+   * @param   string
+   * @return boolean
+   */
+  public function password_matches($password)
+  {
+    # descript account password
+    $account_password = $this->encryption->decrypt($this->auth->account('password'));
+
+    # check if passoword matches account password
+    if($account_password != $password)
+    {
+      $this->form_validation->set_message('password_matches', 'The {field} field does not match account passowrd.');
+
+      return false;
+    }
+
+    return true;
+  }
+
 }
--- application/views/account/update.php
+++ application/views/account/update.php
@@ -10,6 +10,22 @@
     </strong>
   </div>
   <div class="col-lg-6 offset-lg-3 mt-5">
+    <?php if($this->session->flashdata('update_account_error')): ?>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Warning!</strong> <?php echo $this->session->flashdata('update_account_error'); ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    <?php endif; ?>
+    <?php if($this->session->flashdata('account_updated')): ?>
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> <?php echo $this->session->flashdata('account_updated'); ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    <?php endif; ?>
     <!-- ##### Change User Details Form ##### -->
     <ul class="list-group mb-5">
       <?php echo form_open('dashboard/my/account'); ?>
@@ -20,9 +36,11 @@
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Profile Picture</label>
              <div class="col-sm-9">
                <input type="file"
+                      name="picture"
                       style="position: relative; top: 5px;"
-                      class="form-control-file"
+                      class="form-control-file is-invalid"
                       id="exampleFormControlFile1">
+                <?php echo form_error('picture', '<p class="invalid-feedback">', '</p>'); ?>
              </div>
            </div>
         </li>
@@ -32,6 +50,7 @@
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Username</label>
              <div class="col-sm-9">
                <input type="text"
+                      name="username"
                       class="form-control form-control-md"
                       id=""
                       value="<?php echo $this->auth->account('username'); ?>"
@@ -46,6 +65,7 @@
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Email</label>
              <div class="col-sm-9">
                <input type="email"
+                      name="email"
                       class="form-control form-control-md"
                       id=""
                       value="<?php echo $this->auth->account('email'); ?>"
@@ -59,11 +79,13 @@
           <div class="form-group row">
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Name</label>
              <div class="col-sm-9">
-               <input type="email"
+               <input type="text"
+                      name="name"
                       class="form-control form-control-md"
                       id=""
-                      value="<?php echo $this->auth->account('name'); ?>"
+                      value="<?php echo empty(set_value('name')) ? $this->auth->account('name') : set_value('name'); ?>"
                       placeholder="Enter your name.">
+                <?php echo form_error('name', '<p class="invalid-feedback">', '</p>'); ?>
              </div>
            </div>
         </li>
@@ -72,10 +94,11 @@
           <div class="form-group row">
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Surname</label>
              <div class="col-sm-9">
-               <input type="email"
+               <input type="text"
+                      name="surname"
                       class="form-control form-control-md"
                       id=""
-                      value="<?php echo $this->auth->account('surname'); ?>"
+                      value="<?php echo empty(set_value('surname')) ? $this->auth->account('surname') : set_value('surname'); ?>"
                       placeholder="Enter your surname.">
              </div>
            </div>
@@ -89,19 +112,21 @@
       <?php echo form_close(); ?>
     </ul>
     <!-- ##### Change User Password Account #### -->
-    <ul class="list-group mt-5">
+    <ul class="list-group mt-5 mb-5">
       <?php echo form_open('dashboard/my/account'); ?>
+        <input type="hidden" name="type" value="password">
         <li class="list-group-item active"><strong><i class="fas fa-key"></i> Change Password</strong></li>
         <li class="list-group-item pb-0">
           <div class="form-group row">
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Old Password</label>
              <div class="col-sm-9">
-               <input type="email"
-                      class="form-control form-control-md"
+               <input type="password"
+                      name="old_password"
+                      class="form-control form-control-md <?php if(form_error('old_password')) echo 'is-invalid'; ?>"
                       id=""
-                      value=""
-                      placeholder="Account password (old)."
-                      disabled>
+                      value="<?php echo set_value('old_password'); ?>"
+                      placeholder="Account password (old).">
+                <?php echo form_error('old_password', '<strong class="invalid-feedback">', '</strong>'); ?>
              </div>
            </div>
         </li>
@@ -110,11 +135,13 @@
           <div class="form-group row">
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">News Password</label>
              <div class="col-sm-9">
-               <input type="email"
-                      class="form-control form-control-md"
+               <input type="password"
+                      name="new_password"
+                      class="form-control form-control-md <?php if(form_error('new_password')) echo 'is-invalid'; ?>"
                       id=""
                       value=""
                       placeholder="New password.">
+                <?php echo form_error('new_password', '<strong class="invalid-feedback">', '</strong>'); ?>
              </div>
            </div>
         </li>
@@ -123,11 +150,13 @@
           <div class="form-group row">
              <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-md font-weight-bolder">Confirm Password</label>
              <div class="col-sm-9">
-               <input type="email"
-                      class="form-control form-control-md"
+               <input type="password"
+                      name="confirm_password"
+                      class="form-control form-control-md <?php if(form_error('confirm_password')) echo 'is-invalid'; ?>"
                       id=""
                       value=""
                       placeholder="Confirm new pasword.">
+                <?php echo form_error('confirm_password', '<strong class="invalid-feedback">', '</strong>'); ?>
              </div>
            </div>
         </li>
